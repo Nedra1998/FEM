2105ccdda6d9ff6d383d9fbfea9185b126a2ee41
Arden Rasmussen <ardenisthebest@gmail.com>
2019-05-07 10:34:52 -0700

    Compleated writup

---
+++ spring/paper/snippets/r1.lua
@@ -0,0 +1,14 @@
+mesh = "../pslg/rect.poly"
+mesh_angle = 20
+mesh_area = 0.01
+A = {{1, 0}, {0, 1}}
+B = {0, 0}
+C = 0
+bndry = {}
+function soln(x, y)
+  return math.sin(2*x) * math.sin(2*y)
+end
+bndry[0] = soln
+function force(x, y)
+  return -8*math.sin(2*x)*math.sin(2*y)
+end
